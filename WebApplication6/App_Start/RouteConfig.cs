2f82ed1efcc71e6367eda96c69b4f8b8ffb16a26
fzp <fzp@DESKTOP-HD39HVG>
2021-01-08 17:44:43 +0800

    First

--- WebApplication6/App_Start/RouteConfig.cs
+++ WebApplication6/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication6
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Gas", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

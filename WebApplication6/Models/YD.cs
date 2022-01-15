588f05a521fbbea1396ab443f84abbf9ff955bfc
arst8325746 <arst8325746@gmail.com>
2022-01-15 21:28:19 +0800

    加入專案檔案。

---
+++ WebApplication6/Models/YD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class YD
+    {   
+        [DisplayName("款項編號")]
+        public int Id { get; set; }
+
+        [DisplayName("電子發票號碼")]
+        public string Number { get; set; }
+
+        [DisplayName("站名")]
+        [Required(ErrorMessage = "請輸入內容")]
+        public string Gas { get; set; }
+
+
+        [DisplayName("款項")]
+        public string Content { get; set; }
+
+        [DisplayName("付款")]
+        [Required(ErrorMessage = "請輸入內容")]
+        public String Pay { get; set; }
+
+        [DisplayName("金額")]
+        [Required(ErrorMessage = "請輸入內容")]
+        public int Cost { get; set; }
+
+        [DisplayName("月份")]
+        [Required(ErrorMessage = "請輸入內容")]
+        public string Date { get; set; }
+    }
+
+   
+    
+}
